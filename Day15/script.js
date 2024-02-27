7cb35489927170898dc9dc268beb6554371d0b1a
M <557844@cognizant.com>
2024-02-27 16:15:02 +0530

    first commit

---
+++ Day15/script.js
@@ -0,0 +1,7 @@
+const firstName = 'Sathya';
+const last_name = 'Priya';
+let address = "26 Pachiyappan Street, Chennai";
+address = "first street";
+console.log(firstName);
+console.log(last_name);
+console.log(address);
